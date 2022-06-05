2791f870e3acba46e76ffba85094980bd35915fc
kimisoo20 <106228544+kimisoo20@users.noreply.github.com>
2022-06-05 22:27:48 +0800

    v1.2( Classes.Operators Basic)

--- Practice_CSharp1/Program.cs
+++ Practice_CSharp1/Program.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 
 namespace Practice_CSharp1
 {
+
   class Program
     {
         static void Main(string[] args)
         {
 
             //Declare string message 
-            string message = "Hello World!!";
+            string message = "Hello There, Welcome to C# Coding !!";
             //call ouput message
             Console.WriteLine(message);
 
@@ -27,9 +28,19 @@ namespace Practice_CSharp1
             Console.WriteLine("Your name is: " + name);
             Console.WriteLine("Your age is: " + age);
 
+            //Using Operator 
+            var a = 10;
+            var b = 5;
 
+            Console.WriteLine("\nAddition is : " + (a + b));
+            Console.WriteLine("Substraction is : " + (a - b));
+            Console.WriteLine("Multiplication  is : " + (a * b));
+            Console.WriteLine("Division is : " + (a / b));
 
-
+            var KIMI = new anime();
+            KIMI.Series = "Jujutsu Kaisen";
+            KIMI.Movie = "Jujutsu Kaisen 0";
+            KIMI.Choices();
 
 
         }
---
+++ Practice_CSharp1/anime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practice_CSharp1
+{
+    //Create new Class
+    class anime
+    {
+        public string Series;
+        public string Movie;
+
+        //Create Method
+        public void Choices()
+        {
+            Console.WriteLine(" \nWhich one is better " + Series + " or " + Movie);
+        }
+        
+    }
+}
